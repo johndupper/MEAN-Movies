90dfd22b2ad8385325e83dcd991e9aa8b3f3fb77
John Dupper <john.dupper@me.com>
2016-12-22 20:02:23 -0500

    added movie model and router to app

--- app.js
+++ app.js
@@ -6,42 +6,39 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 
-// require mongoose, promise
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var movies = require('./routes/movies');
+
 
 var app = express();
 
 
-
-// Connect to database
 if (process.env.MONGODB_URI) {
     mongoose.connect(process.env.MONGODB_URI);
-}
-else {
+} else {
     mongoose.connect('mongodb://localhost/mean-movies');
 }
+
 mongoose.connection.on('error', function(err) {
         console.error('MongoDB connection error: ' + err);
         process.exit(-1);
     }
 );
+
 mongoose.connection.once('open', function() {
-    console.log("Mongoose has connected to MongoDB!");
+    console.log("mongoose has connected to mongoDB");
 });
 
 
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -50,6 +47,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+// json data route, no views being rendered
+app.use('/api/movies', movies);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -63,7 +62,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
